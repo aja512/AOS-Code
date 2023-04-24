ebbeb2144fb9d130901155e817c39d481838bf85
AMANDA JUDY ANDRADE <42480995+aja512@users.noreply.github.com>
2023-04-24 14:54:58 -0700

    Create seller.c

---
+++ PROJECT 3/seller.c
@@ -0,0 +1,18 @@
+/*
+// COEN 383 AOS Project 3
+// File: seller.c
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "customer.h"
+#include "seller.h"
+
+void *seller(void *seller_args) {
+    seller_args_t args = *((seller_args_t *) seller_args);
+
+    printf("{%s} id: %d, priority: %d\n", args.name, args.id, args.priority);
+
+    return NULL;
+}
