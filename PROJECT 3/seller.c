ed1719ebd69f444d4f95d7b8e9e86622a9947a4c
AMANDA JUDY ANDRADE <42480995+aja512@users.noreply.github.com>
2023-04-29 12:17:40 -0700

    Update seller.c

--- PROJECT 3/seller.c
+++ PROJECT 3/seller.c
@@ -11,8 +11,37 @@
 
 void *seller(void *seller_args) {
     seller_args_t args = *((seller_args_t *) seller_args);
-
     printf("{%s} id: %d, priority: %d\n", args.name, args.id, args.priority);
 
+    // initialize seller's internal clock
+    int sim_hour = 0;       // simulation clock hour value, 0
+    int sim_tenmin = 0;     // simulation clock ten minutes value, 0-5
+    int sim_onemin = 0;     // simulation clock one minute value, 0-9
+
+    // Continue for the full hour
+    for (int i=0; i<60; i++)
+    {
+        if(args.queue->buf[0].arrival_time <= i)    // handle customer that has arrived at front of queue
+        {
+            //printf("Time is %d ", i);
+            customer_t current = dequeue_customer(args.queue);
+            printf("[%d:%d%d]", sim_hour, sim_tenmin, sim_onemin);
+            print_customer(current);
+        }
+        if (args.queue->size == 0) break;   // end early if seller finishes handling all customers
+
+        // update timestamp
+        sim_onemin++;
+        if (sim_onemin >= 10)
+        {
+            sim_tenmin = sim_tenmin + (sim_onemin/10);
+            sim_onemin = sim_onemin % 10;
+            if (sim_tenmin >= 10)
+            {
+                sim_tenmin = sim_tenmin % 10;
+                sim_hour++;
+            }
+        }
+    }
     return NULL;
 }
